98ebf4b2b32ccd90b7e2470f3e4fe41ead94298e
Igor Knop <igorknop@gmail.com>
2024-02-29 22:14:14 -0300

    fix mapped key

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,16 +1,17 @@
 const keys = document.querySelectorAll('.key');
 
 const audio = document.createElement('audio');
-
+const mappedKeys = [];
 
 keys.forEach(key => {
   key.addEventListener('click', () => {
-    console.log(key.dataset.key);
+    mappedKeys.push(key.dataset.key);
     playTune(key.dataset.key);
   });
 });
 
 function playTune(key) {
+  if (!mappedKeys.includes(key)) { return; };
   audio.src = `./public/${key}.wav`;
   audio.play();
 }
