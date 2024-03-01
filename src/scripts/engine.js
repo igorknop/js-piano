19e2ac8da8d1ca4fa18d2bc02b03411795c5c965
Igor Knop <igorknop@gmail.com>
2024-02-29 22:11:14 -0300

    add active status on keydown

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -14,3 +14,12 @@ function playTune(key) {
   audio.src = `./public/${key}.wav`;
   audio.play();
 }
+
+document.addEventListener('keydown', e => {
+  playTune(e.key);
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
+  key.classList.add('active');
+  setTimeout(() => {
+    key.classList.remove('active');
+  }, 150);
+});
